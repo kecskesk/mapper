0caa75d0b4541c078de21075588f641cd11e0b3a
Kecskes Krisztian <kecskesk@sonrisa.hu>
2018-08-09 21:30:53 +0200

    Spread logic implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MapReaderService } from './map-reader.service';
+import {MapData, MapReaderService} from './map-reader.service';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,17 @@ import { MapReaderService } from './map-reader.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  map = null;
-  mapData = [[1,1,'1d20'], [10,22,'3d4'], [6,24,'3d4'], [17, 28,'3d4'], [6,24,'3d4'], [5,7,'3d4']];
-  title = 'mapper';
+  map: Array<Array<TimestampedColor>> = null;
+  mapData: Array<MapData> = [];
+
+  private rowNumber = 17;
+  private colNumber = 28;
+  private growScale = 1;
 
   constructor(private mapReaderService: MapReaderService) {
     this.initGrid();
     this.mapReaderService.getJSON().subscribe(data => {
-      console.log(data);
+      this.mapData = data;
     });
   }
 
@@ -22,25 +25,166 @@ export class AppComponent {
     this.readMapData();
   }
 
+  grow() {
+    this.map = null;
+    this.growScale = 3;
+    this.colNumber *= this.growScale;
+    this.rowNumber *= this.growScale;
+    this.initGrid();
+    this.readMapData();
+  }
+
+  spread() {
+    let startTime = new Date().getTime();
+
+    for (let rowIdx = 0; rowIdx < this.rowNumber; rowIdx++) {
+      for (let colIdx = 0; colIdx < this.colNumber; colIdx++) {
+        if (this.map[rowIdx][colIdx].color.length > 0 && this.map[rowIdx][colIdx].timestamp < startTime) {
+          let color = this.map[rowIdx][colIdx].color;
+          this.spreadRec(rowIdx + 1, colIdx, color);
+          this.spreadRec(rowIdx - 1, colIdx, color);
+          this.spreadRec(rowIdx, colIdx + 1, color);
+          this.spreadRec(rowIdx, colIdx - 1, color);
+        }
+      }
+    }
+  }
+
+  private spreadRec(rowIdx: number, colIdx: number, color: string) {
+    if (this.isSafeArrayAccess(rowIdx, colIdx) && this.map[rowIdx][colIdx].color.length === 0) {
+      this.map[rowIdx][colIdx].color = color;
+    }
+  }
+
+  private isSafeArrayAccess(rowIdx: number, colIdx: number): boolean {
+    return rowIdx >= 0 && colIdx >= 0 && rowIdx < this.rowNumber && colIdx < this.colNumber;
+  }
+
   private readMapData() {
     this.mapData.forEach(input => {
-      if (input.length !== 3) { return; }
-      let row = input[0] as number - 1;
-      let col = input[1] as number - 1;
-      let data = input[2];
-      this.map[row][col] = data;
+      let row = input.row as number - 1;
+      let col = input.col as number - 1;
+
+      for (let blockRowIdx = 0; blockRowIdx < this.growScale; blockRowIdx++) {
+        for (let blockColIdx = 0; blockColIdx < this.growScale; blockColIdx++) {
+          let newData = new TimestampedColor();
+          newData.color = this.getColor(input);
+          this.map[row * this.growScale + blockRowIdx][col * this.growScale + blockColIdx] = newData;
+        }
+      }
     });
   }
 
   private initGrid() {
     this.map = [];
-    for (let rowIdx = 0; rowIdx < 17; rowIdx++) {
+    for (let rowIdx = 0; rowIdx < this.rowNumber; rowIdx++) {
       let row = [];
-      for (let colIdx = 0; colIdx < 28; colIdx++) {
-        let cell = '';
-        row.push(cell);
+      for (let colIdx = 0; colIdx < this.colNumber; colIdx++) {
+        let newData = new TimestampedColor();
+        newData.color = '';
+        row.push(newData);
       }
       this.map.push(row);
     }
+  }
+
+  private getColor(data: MapData) {
+    switch (data.valueCategory) {
+      case 'd4':
+        if (data.value === 3) {
+          return 'darkgreen';
+        } else if (data.value === 4) {
+          return 'lightgrey';
+        } else {
+          return 'lightgreen';
+        }
+      case 'd6':
+        if (data.value >= 1 && data.value <= 3) {
+          return 'lightgreen';
+        } else if (data.value === 4) {
+          return 'darkgreen';
+        } else {
+          return 'lightgrey';
+        }
+      case 'd8':
+        if (data.value >= 1 && data.value <= 4) {
+          return 'lightgreen';
+        } else if (data.value >= 5 && data.value <= 6) {
+          return 'darkgreen';
+        } else if (data.value === 7) {
+          return 'lightgrey';
+        } else {
+          return 'cyan';
+        }
+      case 'd10':
+        if (data.value >= 1 && data.value <= 3) {
+          return 'lightgreen';
+        } else if (data.value >= 4 && data.value <= 6) {
+          return 'darkgreen';
+        } else if (data.value >= 7 && data.value <= 8) {
+          return 'lightgrey';
+        } else if (data.value === 9) {
+          return 'cyan';
+        } else {
+          return 'brown';
+        }
+      case 'd12':
+        if (data.value >= 1 && data.value <= 3) {
+          return 'lightgreen';
+        } else if (data.value >= 4 && data.value <= 6) {
+          return 'darkgreen';
+        } else if (data.value >= 7 && data.value <= 8) {
+          return 'lightgrey';
+        } else if (data.value >= 9 && data.value <= 10) {
+          return 'cyan';
+        } else if (data.value === 11) {
+          return 'brown';
+        } else {
+          return 'orange';
+        }
+      case 'd20':
+        if (data.value >= 1 && data.value <= 5) {
+          return 'lightgreen';
+        } else if (data.value >= 6 && data.value <= 8) {
+          return 'darkgreen';
+        } else if (data.value >= 9 && data.value <= 11) {
+          return 'lightgrey';
+        } else if (data.value >= 12 && data.value <= 14) {
+          return 'cyan';
+        } else if (data.value >= 15 && data.value <= 17) {
+          return 'brown';
+        } else if (data.value >= 18 && data.value <= 19) {
+          return 'orange';
+        } else {
+          return 'black';
+        }
+      case 'd100':
+        if (data.value >= 10 && data.value <= 30) {
+          return 'lightgreen';
+        } else if (data.value >= 40 && data.value <= 60) {
+          return 'darkgreen';
+        } else if (data.value >= 70 && data.value <= 80) {
+          return 'lightgrey';
+        } else if (data.value === 90) {
+          return 'cyan';
+        } else {
+          return 'brown';
+        }
+    }
+    return 'red';
+  }
+}
+
+export class TimestampedColor {
+  _color: string;
+  timestamp: number;
+
+  get color(): string {
+    return this._color;
+  }
+
+  set color(color: string) {
+    this._color = color;
+    this.timestamp = new Date().getTime();
   }
 }
--- src/app/map-reader.service.ts
+++ src/app/map-reader.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from '../../node_modules/rxjs';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,31 @@ export class MapReaderService {
   constructor(private http: HttpClient) {
   }
 
-  public getJSON(): Observable<any> {
-    return this.http.get('./assets/map0803_201403_01.json');
+  public getJSON(): Observable<Array<MapData>> {
+    return this.http.get('./assets/map0803_201403_01.json').pipe(
+      map((rawInput) => {
+        let inputArray = [];
+        Object.keys(rawInput).forEach((key) => inputArray = rawInput[key]);
+        return inputArray.map((input) => {
+          let newData = new MapData();
+          newData.col = input[0];
+          newData.row = input[1];
+
+          let inputText = input[2].split('d');
+          if (inputText.length !== 2) {
+            throw new Error('bad input - not num d num');
+          }
+          newData.value = inputText[0] as number;
+          newData.valueCategory = 'd' + inputText[1];
+          return newData;
+        });
+    }));
   }
+}
+
+export class MapData {
+  row: number;
+  col: number;
+  value: number;
+  valueCategory: string;
 }
